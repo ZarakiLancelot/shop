de1cbcf15133d93b769777e2f2b2eda4a87c306c
ZarakiLancelot <kenpachi2783@gmail.com>
2022-07-28 03:06:45 -0600

    configuración de rutas, se agrega flash messages, se agrega turbo_stream para las peticiones

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get "users", to: "devise/session#new"
+  end
+
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
